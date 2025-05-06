b166f5033d2bd75c5ede682ee581e5f12ad00aa2
Diego Hinagas <hinagasrodriguez@gmail.com>
2025-05-05 20:52:27 -0400

    feat: created user card

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,17 @@ function App() {
         </div>
    
         <div className='row-start-1 row-end-4 mx-5'>
-          Content
+          <h2 className='text-xl mb-4 text-zinc-800'>Usuarios</h2>
+
+          <div className='border border-zinc-500 rounded-xl py-4 px-5 flex gap-4 text-zinc-800'>
+            <div className='bg-zinc-300 rounded-full w-12 text-center h-12 flex items-center justify-center'>
+              <span className='text-2xl font-light'>J</span>
+            </div>
+            <div>
+              <h3 className='text-lg'>James Baker</h3>
+              <p className='text-zinc-650 text-sm'>jamesbaker@email.com</p>
+            </div>
+          </div>
         </div>
   
         <nav className='border-t-1 border-zinc-400 row-start-4'>
